b541561cb64305d19c882a81ca388e39205f2133
Katic Nikola <nikolakatic1@gmail.com>
2019-05-23 01:10:34 +0200

    Updates

---
+++ precache-manifest.23ded51055ed7f8d449c36af63ee88ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90eb6aa7125cd1464271ec8590ea5991",
+    "url": "/ContactMenager/index.html"
+  },
+  {
+    "revision": "47ff26ebb73877fb1517",
+    "url": "/ContactMenager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a59c7adf19afa1fa342a",
+    "url": "/ContactMenager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "47ff26ebb73877fb1517",
+    "url": "/ContactMenager/static/js/2.03fd6fea.chunk.js"
+  },
+  {
+    "revision": "a59c7adf19afa1fa342a",
+    "url": "/ContactMenager/static/js/main.84b82dc4.chunk.js"
+  },
+  {
+    "revision": "f78f55aca7e4b95069c3",
+    "url": "/ContactMenager/static/js/runtime~main.398fd5d5.js"
+  }
+]);
